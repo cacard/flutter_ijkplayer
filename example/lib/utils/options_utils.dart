e90fb8c85c17f8b2659e047e442de705654111e7
cacard <cacard@126.com>
2020-12-04 14:37:19 +0800

    rtsp h264 yuvj420p OK

--- example/lib/utils/options_utils.dart
+++ example/lib/utils/options_utils.dart
@@ -11,14 +11,17 @@ class OptionUtils {
 
   static Set<IjkOption> createIJKOptions() {
     return <IjkOption>[
-      IjkOption(IjkOptionCategory.player, "mediacodec", 1),
-      IjkOption(IjkOptionCategory.player, "mediacodec-hevc", 1),
+      IjkOption(IjkOptionCategory.player, "mediacodec", 1), ///打开硬解
+      IjkOption(IjkOptionCategory.player, "mediacodec-hevc", 0),//0
       IjkOption(IjkOptionCategory.player, "videotoolbox", 1),
-      IjkOption(IjkOptionCategory.player, "opensles", 0),
+      IjkOption(IjkOptionCategory.player, "opensles", 2),//1
+      IjkOption(IjkOptionCategory.player, "mediacodec-handle-resolution-change", 1),
+      IjkOption(IjkOptionCategory.player, "mediacodec-auto-rotate", 1),
       IjkOption(IjkOptionCategory.player, "overlay-format", 0x32335652),
       IjkOption(IjkOptionCategory.player, "framedrop", 1),
       IjkOption(IjkOptionCategory.player, "start-on-prepared", 0),
       IjkOption(IjkOptionCategory.format, "http-detect-range-support", 0),
+      IjkOption(IjkOptionCategory.format, "ijkapplication", 547488968448),
       IjkOption(IjkOptionCategory.codec, "skip_loop_filter", 48),
     ].toSet();
   }
